4f747e474edb28030b1b7c1f17710abfd55f11d9
miazi2003 <yeasinmiazi1997@gmail.com>
2025-05-18 14:35:52 +0600

    env updated

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
+
 const app = express();
+
+require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3crt5al.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+ 
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("coffee server is being ready");
